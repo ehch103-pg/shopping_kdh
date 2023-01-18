8c23470a1d7845583589294234879aa7ed754dec
ehch103 <ehch103@gmail.com>
2023-01-18 17:24:13 +0900

    2023-01-18 프로젝트 생성

---
+++ shopping/src/main/java/co/kr/shopping/ShoppingApplication.java
@@ -0,0 +1,13 @@
+package co.kr.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingApplication.class, args);
+	}
+
+}
